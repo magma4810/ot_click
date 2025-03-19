33184614dc49eee886bb53cf8d8c30c8e93c5b1e
Pavel <magma4810@gmail.com>
2025-03-19 23:03:18 +0300

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Signin } from "./components/Signin";
 import { FC } from "react";
 
 export const App: FC = () => {
   return (
     <>
-      <BrowserRouter basename="/ot_click">
-      <Routes>
-        <Route path="/signin" element={<Signin />} />
-      </Routes>
-    </BrowserRouter>
+      <HashRouter>
+        <Routes>
+          <Route path="signin" element={<Signin />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 };
