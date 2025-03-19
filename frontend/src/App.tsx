be9f8bc153759d74187494fa3f385aa47c9867ff
Pavel <magma4810@gmail.com>
2025-03-19 13:26:05 +0300

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Signin } from './components/Signin'
+
+export const App = () =>{
+
+  return (
+    <>
+      <Signin/>
+    </>
+  )
+}
+
---
+++ frontend/src/components/Signin.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+export const Signin: FC = () => {
+    return (
+        <div>Hello</div>
+    )
+}
