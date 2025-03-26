922f08ca3d11a5db9402480d8617ffaa9dbfb508
Pavel <magma4810@gmail.com>
2025-03-26 22:06:47 +0300

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ot_click/",
+  base: process.env.NODE_ENV === 'production' ? '/ot_click/' : '/',
   server: {
     proxy: {
       "/api": {
